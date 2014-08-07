f1facc40f35b063758adfedbd051c5fde0f118e1
robinkun <robinkun96@gmail.com>
2014-08-07 15:10:25 +0900

    rename func to util

---
+++ sort/util.cpp
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include "func.h"
+
+// 範囲内にいるかチェック
+int check_in_scope(int width, int height, int x, int y) {
+  if(x >= 0 && x < width && y >= 0 && y < height)
+    return 1;
+  else return 0;
+}
+
+void disp_level_line(int num) {
+  int i;
+
+  printf("+");
+  for(i = 0; i < num; i++) printf("-");
+  printf("+");
+  puts("");
+}
+
+void surroundings(int *x, int *y, int direction) {
+  switch(direction) {
+  case UP: // 
+    (*y)--;
+    break;
+  case RIGHT: //  
+    (*x)++;
+    break;
+  case DOWN: //
+    (*y)++;
+    break;
+  case LEFT: //  
+    (*x)--;
+    break;
+  }
+}
+
+int min(int num1, int num2) {
+  if(num1 <= num2) return num1;
+  if(num2 < num1) return num2;
+  return 0;
+}
+
+int max(int num1, int num2) {
+  if(num1 >= num2) return num1;
+  if(num2 > num1) return num2;
+  return 0;
+}
+
+void change_num(int *num1, int *num2) {
+  int dummy = *num1;
+  *num1 = *num2;
+  *num2 = dummy;
+}
+
+int convert_num(int x, int y) {
+  return x + y * 0x10;
+}
+
+// 数字を列の数字に変換
+int convert_y(int num) {
+  return (num / 0x10);
+}
+
+// 数字を行の数字に変換
+int convert_x(int num) {
+  return (num % 0x10);
+}
---
+++ sort/util.h
@@ -0,0 +1,18 @@
+#define UP 0
+#define RIGHT 1
+#define DOWN 2
+#define LEFT 3
+#ifndef INCLUDED_FUNC_H
+#define INCLUDED_FUNC_H
+
+extern int check_in_scope(int width, int height, int x, int y);
+extern void disp_level_line(int num);
+extern void surroundings(int *x, int *y, int direction);
+extern int min(int num1, int num2);
+extern int max(int num1, int num2);
+extern void change_num(int *num1, int *num2);
+extern int convert_num(int x, int y);
+extern int convert_y(int num);
+extern int convert_x(int num);
+
+#endif
