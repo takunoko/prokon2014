e95e222c2c06a9e022d998f311d1ec5780cc961f
virus <virusbb001a@gmail.com>
2014-09-29 22:54:19 +0900

    例外処理修正

--- httpClient/test.cpp
+++ httpClient/test.cpp
@@ -9,22 +9,24 @@ using namespace std;
 
 int main(){
  ProkonClient client;
- ofstream ofs("./downloaded.ppm");
  string res;
  try {
-  res=client.getProbrem(1);
+  res=client.getProbrem(0);
   
- } catch (char * exception) {
+ } catch (char const * exception) {
 
   cerr << "Exception: " << exception << endl;
   exit(EXIT_FAILURE);
  }
+ cout << res << endl;
  vector<char> v(res.begin(),res.end());
 
+#if 0
  cv::Mat pic;
  pic=cv::imdecode(cv::Mat(v),-1);
  cv::namedWindow("testWindow",CV_WINDOW_AUTOSIZE);
  cv::imshow("testWindow",pic);
  cv::waitKey(0);
+#endif
  return 0;
 }
