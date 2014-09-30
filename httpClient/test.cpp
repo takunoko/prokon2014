1461fb769fbfc8c81b1390544bee088be30437d2
virus <virusbb001a@gmail.com>
2014-09-30 18:43:29 +0900

    回答送信処理追加 なにしているかの表示

--- httpClient/test.cpp
+++ httpClient/test.cpp
@@ -10,16 +10,18 @@ using namespace std;
 int main(){
  ProkonClient client;
  string res;
+
+ cout << "Picture Downloading" << endl;
  try {
   // 引数に問題番号を指定して画像のバイナリstringを返す(cv::Mat形式にするかも)
-  res=client.getProbrem(0);
+  res=client.getProblem(1);
   
  } catch (char const * exception) {
 
   cerr << "Exception: " << exception << endl;
   exit(EXIT_FAILURE);
  }
- cout << res << endl;
+ //cout << res << endl;
  vector<char> v(res.begin(),res.end());
 
 #if 0
@@ -29,5 +31,18 @@ int main(){
  cv::imshow("testWindow",pic);
  cv::waitKey(0);
 #endif
+
+ cout <<"Answer Send" << endl;
+
+ try {
+  // 引数に問題番号を指定して画像のバイナリstringを返す(cv::Mat形式にするかも)
+  res=client.sendAnswer(1,"1145148938101919889466364364");
+  
+ } catch (char const * exception) {
+
+  cerr << "Exception: " << exception << endl;
+  exit(EXIT_FAILURE);
+ }
+ cout << res << endl;
  return 0;
 }
