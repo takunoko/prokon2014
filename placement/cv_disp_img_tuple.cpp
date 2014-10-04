5c5398417b29eb86ce73c2dc5e0b53fb13df36f8
takunoko <takunoko@gmail.com>
2014-10-05 02:10:05 +0900

    change declaration cost & cost_t

--- placement/cv_disp_img_tuple.cpp
+++ placement/cv_disp_img_tuple.cpp
@@ -9,22 +9,23 @@
 #include <tuple>
 
 //使ってない？
-//#include <stdlib.h>
-//#include <math.h> /* abs 絶対値 */
-
 // openCV関連
 // Makefileの方でライブラリとかの場所を指定してある
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
 
-//画像ファイル名と分割数
-//後々、分割数をファイルから読み込む
+// 画像ファイル名と分割数
+// 後々、分割数をファイルから読み込む
 #define FILENAME "../pic_data/9.ppm"
 #define PIECE_X 4
 #define PIECE_Y 2
 
-//XY座標を1次元に変換する
+#define ORIGIN_IMG 0
+#define LINE_IMG 1
+
+// 2次元を1次元に
 #define CONV_XY( x, y)	( x + y*PIECE_X )
+// XY座標を1次元に
 #define CONV_X(XY) (XY % PIECE_X)
 #define CONV_Y(XY) (XY / PIECE_X)
 
@@ -44,12 +45,18 @@ void disp_img();
 
 class PPMFILE{
 	private:
-		string img_name;
-		cv::Mat origin_img;
 		int part_size_x, part_size_y;	//左右どれだけのピース数があるか?
 
+		string img_name;
+		cv::Mat origin_img;
+		cv::Mat	line_img;
 		vector<cv::Mat>	part_img;
-		//vector<string> file;
+
+		// cost配列(3次元)
+		vector< vector< vector< pair<int,int> > > > cost;
+		// cost_t配列(1次元)
+		vector<COST_TUPLE> cost_t;
+
 	public:
 		PPMFILE(string filename){
 			//file.push_back(filename);
@@ -57,18 +64,26 @@ class PPMFILE{
 			origin_img = cv::imread(img_name, -1);
 			part_size_x = PIECE_X;
 			part_size_y = PIECE_Y;
+			// costのリサイズ
+
 			//それぞれのピースに分割
 			this->create_partition();
 		}
 		//画像を表示する
-		void disp_img(void){
+		void disp_img(int type){
 			cv::namedWindow("image", CV_WINDOW_AUTOSIZE | CV_WINDOW_FREERATIO);
-			cv::imshow("image", origin_img);
-			cv::waitKey(0);	//waitKey(0)で何か入力するまで処理を停止
+			switch (type){
+				case ORIGIN_IMG:	cv::imshow("image", origin_img);
+								break;
+				case LINE_IMG: cv::imshow("image", line_img);
+								break;
+				default:
+								break;
+			}
 		}
 		//境界線を引く
 		void write_line(void){
-			cv::Mat line_img = origin_img.clone(); // origin_img をコピー　
+			line_img = origin_img.clone(); // origin_img をコピー　
 			//縦向きの線
 			for(int x=1; x < part_size_x; x++){
 				cv::line( line_img, cv::Point( x*(line_img.cols/part_size_x), 0), cv::Point( x*(line_img.cols/part_size_x), line_img.rows), cv::Scalar( 200, 0, 0), 2, 0);
@@ -77,9 +92,6 @@ class PPMFILE{
 			for(int y=1; y < part_size_y; y++){
 				cv::line( line_img, cv::Point( 0, y*(line_img.rows/part_size_y)), cv::Point( line_img.cols, y*(line_img.rows/part_size_y)), cv::Scalar( 0, 0, 200), 2, 0);
 			}
-			cv::namedWindow("line_image", CV_WINDOW_AUTOSIZE | CV_WINDOW_FREERATIO);
-			cv::imshow("line_image", line_img);
-			cv::waitKey(0);	//waitKey(0)で何か入力するまで処理を停止
 		}
 		// それぞれのピースに分割
 		void create_partition(void){
@@ -97,14 +109,19 @@ class PPMFILE{
 
 		// 配列の近似値を計算ｎする。
 		void calc_cost(void){
-			// costの宣言
-			vector< vector< vector< pair<int,int> > > > cost( part_size_x*part_size_y, vector<vector<pair<int,int> > >(4, vector<pair<int,int> >( part_size_x*part_size_y)));
+			// costのサイズ変更
+			cost.resize( part_size_x*part_size_y);
+			for(int i=0; i<part_size_x*part_size_y; i++){
+				cost[i].resize(4);
+				for(int j=0; j<4; j++){
+					cost[i][j].resize(part_size_x*part_size_y);
+				}
+			}
+
 			//上下左右のcostを保持
 			// 0:上 1:下 2:左 3:右
 			int cost_tmp[4];
 
-			// tuple配列の宣言
-			vector<COST_TUPLE> cost_t;
 
 			cout << "start_calc" << endl;
 
@@ -172,7 +189,6 @@ class PPMFILE{
 					for(int k=0; k < 4; ++k){
 						cost_t.push_back(make_tuple( cost_tmp[k], abs_xy, xy, k));
 						cost[abs_xy][k][xy] = make_pair( cost_tmp[k], xy);
-						cout << "k " << k << endl;
 					}
 				}
 			}
@@ -180,31 +196,17 @@ class PPMFILE{
 
 			cout << "start_sort" << endl;
 
-			// // 表示
-			// for(int abs_y=0; abs_y < part_size_y; ++abs_y){
-			// 	for(int abs_x=0; abs_x < part_size_x; ++abs_x){
-			// 		// 優先度順
-			// 		cout << "x: " << abs_x << "y: " << abs_y << endl;
-			// 		cout << " u d r l" << endl;
-			// 		for(int i=0; i < part_size_x * part_size_y; i++){
-			// 			cout << " sc:" << cost[CONV_XY( abs_x, abs_y)][0][i].first << " in: " << cost[CONV_XY( abs_x, abs_y)][0][i].second;
-			// 			cout << "| sc:" << cost[CONV_XY( abs_x, abs_y)][1][i].first << " in: " << cost[CONV_XY( abs_x, abs_y)][1][i].second;
-			// 			cout << "| sc:" << cost[CONV_XY( abs_x, abs_y)][2][i].first << " in: " << cost[CONV_XY( abs_x, abs_y)][2][i].second;
-			// 			cout << "| sc:" << cost[CONV_XY( abs_x, abs_y)][3][i].first << " in: " << cost[CONV_XY( abs_x, abs_y)][3][i].second;
-			// 			cout << endl;
-			// 		}
-			// 	}
-			// }
-
 			// 独自のルールでCOST_TUPLEをソート
 			sort( cost_t.begin(), cost_t.end(), my_compare);
 
+			cout << "end calc" << endl;
+		}
+		void disp_cost_list(void){
+			// 表示
 			cout << "score : my_pos : pair_pos : direction " << endl;
 			for(int i=0; i < cost_t.size(); i++){
 				cout << i << " |cost: " << get<0>(cost_t[i]) << ", (" << CONV_X( get<1>(cost_t[i])) << "," << CONV_Y( get<1>(cost_t[i])) << "), (" << CONV_X(get<2>(cost_t[i])) << "," << CONV_Y(get<2>(cost_t[i])) << "), " << get<3>(cost_t[i]) << ", " << endl;
 			}
-
-			cout << "end calc" << endl;
 		}
 };
 
@@ -212,9 +214,12 @@ int main(void){
 	PPMFILE *img1 = new PPMFILE(FILENAME);
 
 	img1->calc_cost();
-	//img1->write_line();
 
-	//cv::waitKey(0);	//waitKey(0)で何か入力するまで処理を停止
+	img1->disp_cost_list();
+	// img1->write_line();
+	// img1->disp_img(LINE_IMG);
+
+	// cv::waitKey(0);	//waitKey(0)で何か入力するまで処理を停止
 
 	return 0;
 }
