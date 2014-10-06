6baf3d37761c502b4ae130d2b85885a8177cd686
virus <virusbb001a@gmail.com>
2014-10-06 15:30:33 +0900

    コメント抜き出し処理追加

--- httpClient/client.h
+++ httpClient/client.h
@@ -1,10 +1,12 @@
-#ifndef __PROKON_CLIENT_H_INCLUDE__
-#define __PROKON_CLIENT_H_INCLUDE__
+#ifndef __PROKON_CLIENT_H_INCLUDED__
+#define __PROKON_CLIENT_H_INCLUDED__
 
 #include <string>
 #include <stdio.h>
 #include <fstream>
 #include <curl/curl.h>
+
+#include "QuestionHeader.hpp"
 
 using namespace std;
 
@@ -30,7 +32,7 @@ class ProkonClient{
   ~ProkonClient();
   static const string SERVER_ADDRESS;
   static const string TEAM_TOKEN;
-  string getProblem(int problemNo);
+  string getProblem(int problemNo,QuestionHeader & data);
   string sendAnswer(int problemNo,string answer);
 };
 #endif
