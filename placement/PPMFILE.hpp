f2b2d1d83a4b5d398c1affff9d06859330c5e438
virus <virusbb001a@gmail.com>
2014-10-10 16:24:38 +0900

    インデント整形(tabに直した) CONV_X,CONV_Y,CONV_XYをインライン関数化

--- placement/PPMFILE.hpp
+++ placement/PPMFILE.hpp
@@ -14,11 +14,6 @@
 #define DIRE_R 2
 #define DIRE_L 3
 
-// 2次元を1次元に
-#define CONV_XY( x, y)	( x + y*PIECE_X )
-// XY座標を1次元に
-#define CONV_X(XY) (XY % PIECE_X)
-#define CONV_Y(XY) (XY / PIECE_X)
 
 #define MIN_2( A, B) ((A) < (B) ? (A) : (B))
 #define BIG_2( A, B) ((A) > (B) ? (A) : (B))
@@ -35,47 +30,59 @@ typedef struct{
 
 
 class PPMFILE{
- private:
-  int part_size_x, part_size_y;	//左右どれだけのピース数があるか?
+	private:
+		int part_size_x, part_size_y;	//左右どれだけのピース数があるか?
 
-  cv::Mat origin_img;
-  cv::Mat	line_img;
-  cv::Mat	result_img;
+		cv::Mat origin_img;
+		cv::Mat	line_img;
+		cv::Mat	result_img;
 
-  vector<cv::Mat>	part_img;
+		vector<cv::Mat>	part_img;
 
-  // cost配列(3次元)
-  vector< vector< vector< pair<int,int> > > > cost;
+		// cost配列(3次元)
+		vector< vector< vector< pair<int,int> > > > cost;
 		vector< vector< vector< pair<int,int> > > > cost_maru;
-  // cost_t配列(1次元)
-  vector<COST_TUPLE> cost_t;
+		// cost_t配列(1次元)
+		vector<COST_TUPLE> cost_t;
 		vector<COST_TUPLE> cost_t_maru;
-  // 配置の位置を示す配列
-  map<int,pair<int,int> > placement_pos;
+		// 配置の位置を示す配列
+		map<int,pair<int,int> > placement_pos;
 
- public:
-  PPMFILE(cv::Mat origin_img_tmp, int piece_x, int piece_y);
-  //画像を表示する
-  void disp_img(int type);
-  //境界線を引く
-  void write_line(void);
-  // それぞれのピースに分割
-  void create_partition(void);
-  // 配列の近似値を計算ｎする。
-  void calc_cost(void);
+	public:
+
+		// 2次元を1次元に
+		inline int CONV_XY(int x,int y){
+			return x+y * part_size_x;
+		}
+		// XY座標を1次元に
+		inline int CONV_X(int XY){
+			return XY % part_size_x;
+		}
+		inline int CONV_Y(int XY){
+			return XY/part_size_x;
+		}
+		PPMFILE(cv::Mat origin_img_tmp, int piece_x, int piece_y);
+		//画像を表示する
+		void disp_img(int type);
+		//境界線を引く
+		void write_line(void);
+		// それぞれのピースに分割
+		void create_partition(void);
+		// 配列の近似値を計算ｎする。
+		void calc_cost(void);
 		// まるさん方式　
 		void calc_cost_maru(void);
 
-  void disp_cost_list(void);
+		void disp_cost_list(void);
 
-  // 配置
-  void placement(void);
+		// 配置
+		void placement(void);
 
-  void disp_placement(void);
+		void disp_placement(void);
 
-  // 配置してみた時の表示
-  void create_result_img(void);
+		// 配置してみた時の表示
+		void create_result_img(void);
 
-  // xy_1のdire方向のxy_2とのcost
-  int get_cost(int xy_1, int xy_2, int dire);
+		// xy_1のdire方向のxy_2とのcost
+		int get_cost(int xy_1, int xy_2, int dire);
 };
