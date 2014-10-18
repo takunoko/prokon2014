3f93bc231ea19f71a456a625eb3c7fcc60f18f69
virus <virusbb001a@gmail.com>
2014-10-18 11:43:07 +0900

    URLのエラーを修正

--- httpClient/client.cpp
+++ httpClient/client.cpp
@@ -15,7 +15,7 @@ using namespace std;
 
 // サーバー
 //const string ProkonClient::SERVER_ADDRESS="172.16.1.2";
-const string ProkonClient::SERVER_ADDRESS="localhost/web2/pic/problem/";
+const string ProkonClient::SERVER_ADDRESS="localhost/web2/pic/";
 // チーム固有トークン
 const string ProkonClient::TEAM_TOKEN="0768109858";
 #define PORT 8080
@@ -58,7 +58,7 @@ string ProkonClient::getProblem(int problemNo,QuestionHeader & data){
  probStr << problemNo;
 
  // URL
- string url="http://" + SERVER_ADDRESS + "/prob" + probStr.str() + ".ppm";
+ string url="http://" + SERVER_ADDRESS + "/problem/prob" + probStr.str() + ".ppm";
 #if DEBUG==1
  cout << url << endl;
 #endif
