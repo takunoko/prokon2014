2eeb9eb3e5dc4af43c7ca6371c4fb0a8a5e60b46
virus <virusbb001a@gmail.com>
2014-10-18 17:41:39 +0900

    受信したデータを表示させる

--- pic_data/SubmitAnswer.pl
+++ pic_data/SubmitAnswer.pl
@@ -10,8 +10,8 @@ my @param = $q->param();
 
 print $q->header(-type => 'text/plain' , -charset=>'utf-8');
 
-print "ACCEPTED 2\n"
+print "ACCEPTED 2\n";
 
-# foreach my $key(@param){
-#  print "$key : " .$q->param($key) ."\n";
-# }
+foreach my $key (@param){
+ print "$key : " .$q->param($key) ."\n";
+}
